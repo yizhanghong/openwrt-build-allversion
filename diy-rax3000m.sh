d2aa7d45dc95afeda54cc5f7ea589997ceebed92
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-02-28 17:40:43 +0800

    Update diy-rax3000m.sh
    modify hostname and WiFi SSID

--- diy-rax3000m.sh
+++ diy-rax3000m.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# 修改默认IP
+# 修改默认IP，主机名，WiFi名称
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-
+sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/CMCCRAX/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
+# function git_sparse_clone() {
+#   branch="$1" repourl="$2" && shift 2
+#   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+#   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+#   cd $repodir && git sparse-checkout set $@
+#   mv -f $@ ../package
+#   cd .. && rm -rf $repodir
+# }
 
 # 添加额外插件
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
