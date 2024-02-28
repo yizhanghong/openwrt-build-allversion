271866202b581e2c83075a228e079c7cfbe625ff
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-02-28 17:20:11 +0800

    Create diy-rax3000m.sh

---
+++ diy-rax3000m.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+
+
+./scripts/feeds update -a
+./scripts/feeds install -a
