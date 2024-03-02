0898707e1d1cee8f3771d75790ae5ed016c3d69e
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-02 23:24:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/L1Pro/g' package/base-files/files/bin/config_generate
+
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+
+
+# 调整 ZeroTier 到 服务 菜单
+sed -i 's/vpn/services/g; s/VPN/Services/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
