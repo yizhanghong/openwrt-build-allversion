78805f747dc34058e195d47cf1b52115477cc6b1
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-04 00:41:37 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i 's/ImmortalWrt/L1Pro/g' package/base-files/files/bin/config_generate
 
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 
 
 
