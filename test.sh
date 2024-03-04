2fbcbd78f50cff17ae464ff65523161a584774a7
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-04 18:43:55 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,13 +8,14 @@ echo >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-echo 'src-git kenzok8small https://github.com/kenzok8/small' >> feeds.conf.default
 ./scripts/feeds update nas nas_luci
 ./scripts/feeds install -a -p nas
 ./scripts/feeds install -a -p nas_luci
 ./scripts/feeds update istore
 ./scripts/feeds install -d y -p istore luci-app-store
 
+sed -i '$a src-git kenzok8small https://github.com/kenzok8/small' feeds.conf.default
+git pull
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
