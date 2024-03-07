53a034b2d1d1be8a14a803e7af814098f20af015
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-07 21:27:17 +0800

    Update armv8.sh

--- armv8.sh
+++ armv8.sh
@@ -7,3 +7,6 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/mypackage/luci-app-poweroff
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/mypackage/luci-app-amlogic
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/mypackage/luci-app-passwall2
+
+./scripts/feeds update -a
+./scripts/feeds install -a
