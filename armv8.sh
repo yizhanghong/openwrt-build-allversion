87d526eed8edd7a3e372dbbfe72cbac6af72356c
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-07 20:46:10 +0800

    Update armv8.sh

--- armv8.sh
+++ armv8.sh
@@ -1,5 +1,5 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+# sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 
 # 添加额外插件
