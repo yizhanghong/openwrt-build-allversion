88e292e05118c7b37914dd1a3911520fd25b0fb8
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-11 23:56:36 +0800

    Update armv8.sh

--- armv8.sh
+++ armv8.sh
@@ -1,5 +1,5 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+# sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 
 # 添加额外插件
@@ -10,11 +10,11 @@ git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/mypack
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/mypackage/luci-app-argon-config
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/mypackage/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/mypackage/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/mypackage/nas-luci
-mv package/mypackage/nas-packages/network/services/* package/mypackage/nas-packages/
-rm -rf package/mypackage/nas-packages/network
+# git clone --depth=1 -b main https://github.com/linkease/istore.git package/mypackage/istore
+# git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/mypackage/nas-packages
+# git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/mypackage/nas-luci
+# mv package/mypackage/nas-packages/network/services/* package/mypackage/nas-packages/
+# rm -rf package/mypackage/nas-packages/network
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
