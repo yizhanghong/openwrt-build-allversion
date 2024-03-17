59bfa8f78967aad81d418e8841d03f012f7515d8
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-03-17 23:59:47 +0800

    Update ax5jdc.sh

--- ax5jdc.sh
+++ ax5jdc.sh
@@ -1,27 +1,3 @@
-#!/bin/sh
-# shellcheck disable=SC2086,SC3043,SC2164,SC2103,SC2046,SC2155
-
-get_sources() {
-  # the checkout actions will set $HOME to other directory,
-  # we need to reset some necessary git configs again.
-  git config --global user.name "OpenWrt Builder"
-  git config --global user.email "buster-openwrt@ovvo.uk"
-
-  git clone $BUILD_REPO --single-branch -b $GITHUB_REF_NAME openwrt
-
-  cd openwrt
-  ./scripts/feeds update -a
-  ./scripts/feeds install -a
-  cd -
-}
-
-echo_version() {
-  echo "[=============== openwrt version ===============]"
-  cd openwrt && git log -1 && cd -
-  echo
-  echo "[=============== configs version ===============]"
-  cd configs && git log -1 && cd -
-}
 
 apply_patches() {
   [ -d patches ] || return 0
@@ -36,33 +12,4 @@ apply_patches() {
   done
 }
 
-build_firmware() {
-  cd openwrt
 
-  cp ${GITHUB_WORKSPACE}/config/ax5jdc .config
-  make -j$(($(nproc) + 1)) V=e || make -j1 V=sc || exit 1
-
-  cd -
-}
-
-package_binaries() {
-  local bin_dir="openwrt/bin"
-  local tarball="${BUILD_PROFILE}.tar.gz"
-  tar -zcvf $tarball -C $bin_dir $(ls $bin_dir -1)
-}
-
-package_dl_src() {
-  [ -n "$BACKUP_DL_SRC" ] || return 0
-  [ $BACKUP_DL_SRC = 1 ] || return 0
-
-  local dl_dir="openwrt/dl"
-  local tarball="${BUILD_PROFILE}_dl-src.tar.gz"
-  tar -zcvf $tarball -C $dl_dir $(ls $dl_dir -1)
-}
-
-get_sources
-echo_version
-apply_patches
-build_firmware
-package_binaries
-package_dl_src
