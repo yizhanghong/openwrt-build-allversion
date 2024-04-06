893887462684c4b36c6be150170de9cd77c55481
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-04-06 16:15:28 +0800

    Update and rename ax5jdc.sh to lean-ax5jdc.sh

--- lean-ax5jdc.sh
+++ lean-ax5jdc.sh
@@ -1,7 +1,5 @@
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 
-./scripts/feeds update -a
-./scripts/feeds install -a
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
