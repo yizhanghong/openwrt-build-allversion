524b0d6e2e3b80d0a804822e132fbfedb17ffab4
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-04-09 15:52:45 +0800

    Update rax3000m.sh

--- rax3000m.sh
+++ rax3000m.sh
@@ -1,5 +1,6 @@
 # 修改默认IP，主机名，WiFi名称
-sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/CMCCRAX/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # sed -i '$d' feeds.conf.default
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/small-package
