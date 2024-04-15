d592cf9a124f36f56f91b8f70ad28189ea39b299
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-04-16 01:49:51 +0800

    Update rax3000m.sh

--- rax3000m.sh
+++ rax3000m.sh
@@ -3,7 +3,13 @@ sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/CMCCRAX/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # sed -i '$d' feeds.conf.default
+# 添加homeproxy
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/small
 mv package/small/chinadns-ng package/
 rm -rf package/small
+# 添加cpufreq和diskman
+git clone --depth=1 https://github.com/immortalwrt/luci.git package/immortal-luci
+mv package/immortal-luci/applications/luci-app-cpufreq package/
+mv package/immortimmortal-lucial-luci/applications/luci-app-diskman package/
+rm -rf package/immortal-luci
