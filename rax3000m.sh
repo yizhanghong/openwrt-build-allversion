47dbead7832d879c6256ba19eda87dc87cf4e21c
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-04-20 14:10:23 +0800

    Update rax3000m.sh

--- rax3000m.sh
+++ rax3000m.sh
@@ -1,7 +1,7 @@
 # 修改默认IP，主机名，WiFi名称
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
-sed -i 's/r1.1/v24.4.16/g' package/emortal/default-settings/files/99-default-settings
-sed -i 's/OpenWRT/OpenWrt/g' package/emortal/default-settings/files/99-default-settings
+# sed -i 's/r1.1/v24.4.16/g' package/emortal/default-settings/files/99-default-settings
+# sed -i 's/OpenWRT/OpenWrt/g' package/emortal/default-settings/files/99-default-settings
 # sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/CMCCRAX/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # sed -i '$d' feeds.conf.default
