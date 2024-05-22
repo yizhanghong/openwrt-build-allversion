b18b06e3d0aff7376e2e153291ab1eb476724166
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-05-22 20:19:16 +0800

    Delete md5 directory

--- md5/generic.sh
+++
@@ -1,4 +0,0 @@
-# 修改默认IP，主机名，WiFi名称
-sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
-sed -i 's/r1.2/v24.5.21/g' package/emortal/default-settings/files/99-default-settings
-sed -i 's/OpenWRT/OpenWrt/g' package/emortal/default-settings/files/99-default-settings
