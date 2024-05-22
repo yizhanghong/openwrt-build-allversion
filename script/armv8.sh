e016ca217f5f89d11ffdcd0fa9e548bf77363b47
Sihao1997 <65899583+Sihao1997@users.noreply.github.com>
2024-05-22 20:16:51 +0800

    Update and rename Makefile to armv8.sh

---
+++ script/armv8.sh
@@ -0,0 +1,14 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+# 添加额外插件
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/mypackage/luci-app-poweroff
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/mypackage/luci-app-amlogic
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/mypackage/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/mypackage/luci-app-argon-config
+
+# iStore
+# git clone --depth=1 -b main https://github.com/linkease/istore.git package/mypackage/istore
+# git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/mypackage/nas-packages
+# git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/mypackage/nas-luci
+# mv package/mypackage/nas-packages/network/services/* package/mypackage/nas-packages/
+# rm -rf package/mypackage/nas-packages/network
