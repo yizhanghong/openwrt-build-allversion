8ebd4655027abdcf6129493e6f47fc039e66717d
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-06-11 18:16:33 +0800

    Update generic.sh

--- script/generic.sh
+++ script/generic.sh
@@ -2,8 +2,9 @@
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 sed -i 's/r1.5/v24.6.11/g' package/emortal/default-settings/files/99-default-settings
 sed -i 's/OpenWRT/OpenWrt/g' package/emortal/default-settings/files/99-default-settings
+sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 #默认WiFi设置
-sed -i 's/OpenWrt/CMCC-8Hd5/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+sed -i 's/LibWrt/CMCC-8Hd5/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 #sed -i 's/disabled=0/disabled=1/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 sed -i 's/encryption=none/encryption=psk2/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 sed -i '214i\\t\t\tset wireless.default_${name}.key=123456qwerty' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
