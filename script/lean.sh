0ee3a5d1da563be8c42b7ba5cafc655b9736d889
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-06-18 20:15:33 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -1,5 +1,10 @@
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 
+rm -rf feeds/packages/net/{mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
 # 修改版本为编译日期
 # date_version=$(date +"%y.%m.%d")
 # orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
@@ -8,7 +13,3 @@ sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_genera
 # Clear the login password
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-# git clone --depth=1 https://github.com/kenzok8/small.git package/mypackage/small
-# rm -rf package/mypackage/small/mosdns
