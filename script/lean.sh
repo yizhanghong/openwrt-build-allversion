d1dadd83317fc2e3c149a1908eb68038557cd41a
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-06-30 21:03:08 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -1,6 +1,6 @@
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 
-rm -rf feeds/packages/net/{mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{mosdns,xray*,v2ray*,v2ray*,sing*}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
