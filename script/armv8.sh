75d403e723cb7b10cfce450e5df9fd40ef91c06f
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-07-16 03:05:48 +0800

    Update armv8.sh

--- script/armv8.sh
+++ script/armv8.sh
@@ -1,5 +1,5 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-# sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 # 添加额外插件
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/mypackage/luci-app-poweroff
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/mypackage/luci-app-amlogic
