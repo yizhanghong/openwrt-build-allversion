2cc011a06d93fce8ff026a0b9a79f9b390c52af7
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-04 17:01:20 +0800

    Update wrt1900acs.sh

--- script/wrt1900acs.sh
+++ script/wrt1900acs.sh
@@ -1,18 +1,1 @@
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-
-# 添加kenzok8_small插件库, 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本 ，可以用以下命令
-rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/kenzok8/golang feeds/packages/lang/golang
-#删除自带的老旧依赖，ssr-plus，passwall
-rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy}
-rm -rf feeds/packages/net/{pdnsd-alt,simple-obfs,sing-box,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns,redsocks2}
-rm -rf feeds/packages/net/{gn,ssocks,shadow-tls}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-mosdns}
-#下载安装kenzok8的small库
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-#安装最新openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
