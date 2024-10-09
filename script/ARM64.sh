70da63b5f9c185d1007eb351e0099521a8f34264
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-09 15:37:29 +0800

    Update ARM64.sh

--- script/ARM64.sh
+++ script/ARM64.sh
@@ -1,17 +1,18 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 # 添加额外插件
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/mypackage/luci-app-poweroff
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/mypackage/luci-app-amlogic
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+git clone --depth=1 https://github.com/kiddin9/openwrt-clouddrive2.git package/luci-app-clouddrive2
 # git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/mypackage/luci-theme-argon
 # git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/mypackage/luci-app-argon-config
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/mypackage/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/mypackage/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/mypackage/nas-luci
-mv package/mypackage/nas-packages/network/services/* package/mypackage/nas-packages/
-rm -rf package/mypackage/nas-packages/network
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
 
 # 添加kenzok8_small插件库, 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本 ，可以用以下命令
 #rm -rf feeds/packages/lang/golang
