d18d1297b31b5505c58014195acc79175ab0932c
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-10 14:58:13 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -1,3 +1,3 @@
-sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/CMCC-RAX/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i 's/ImmortalWrt/Xiaomi/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
