f914da3c8e456c44348fe66d478d5e2f875030ac
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-10 15:02:59 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -1,3 +1,6 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/Xiaomi/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+# sed -i 's/ImmortalWrt/Xiaomi/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
