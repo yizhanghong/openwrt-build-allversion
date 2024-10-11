39a000f2b14fa3507211e562ac47c52962448205
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-12 01:12:50 +0800

    Update q20.sh

--- script/q20.sh
+++ script/q20.sh
@@ -3,6 +3,7 @@ sed -i 's/ImmortalWrt/RedmiAX6s/g' package/base-files/files/bin/config_generate
 
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argonv3
 rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
