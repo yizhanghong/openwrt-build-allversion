542eda7c732e4213aa7ec85d90749f294fa7502d
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-15 14:20:59 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -1,25 +1,29 @@
 # 修改默认IP，主机名
-sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 #默认WiFi设置
 sed -i 's/LiBwrt/IPQ6000/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i 's/encryption=none/encryption=psk2/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i '214i\\t\t\tset wireless.default_${name}.key=123456qwerty' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # iStore
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
 git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
 mv package/nas-packages/network/services/* package/nas-packages/
 rm -rf package/nas-packages/network
+
+#下载5g模块
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+
 #安装最新openclash
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
+# rm -rf feeds/luci/applications/luci-app-openclash
+# git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
 # mv package/openclash/luci-app-openclash feeds/luci/applications/
 # rm -rf package/openclash
 
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone https://github.com/destan19/OpenAppFilter.git package/mypackage/OpenAppFilter
 # mv package/small/chinadns-ng package/mypackage/
 # rm -rf package/small
