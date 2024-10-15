9a5c6d1677b96c3bcc080da6350cb6ac39102c17
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-16 02:12:01 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -11,9 +11,10 @@ sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_genera
 # ./scripts/feeds install -a
 
 # 添加额外插件
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/mypackage/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/applications/luci-app-argon-config
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/themes/luci-theme-argon
 
 git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
