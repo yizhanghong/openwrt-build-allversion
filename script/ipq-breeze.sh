4b75df8edd08b1c54fa57cf6744cad14a7e6e4f6
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-17 01:03:05 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -7,11 +7,11 @@ sed -i 's/LiBwrt/IPQ6000/g' package/network/config/wifi-scripts/files/lib/wifi/m
 # sed -i '214i\\t\t\tset wireless.default_${name}.key=123456qwerty' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-mv package/nas-packages/network/services/* package/nas-packages/
-rm -rf package/nas-packages/network
+# git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+# git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+# git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+# mv package/nas-packages/network/services/* package/nas-packages/
+# rm -rf package/nas-packages/network
 
 #下载5g模块
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
