e247f1bebaa2e5c92ac1d3bc0cecc56d2277d7ea
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-17 14:12:52 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -2,6 +2,8 @@
 # sed -i 's/ImmortalWrt/AX6S/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/AX6S/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
