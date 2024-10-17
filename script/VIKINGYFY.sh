bc15061df9a31ab0907895e50e05352ac4435dc1
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-17 19:45:11 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -1,1 +1,16 @@
-sss
+# 修改默认IP，主机名
+sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+#默认WiFi设置
+sed -i 's/ImmortalWrt/IPQ6000/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+# sed -i 's/encryption=none/encryption=psk2/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+# sed -i '214i\\t\t\tset wireless.default_${name}.key=123456qwerty' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
+
+#下载5g模块
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+
+rm -rf feeds/packages/lang/ruby
+git clone --depth=1 -b openwrt-23.05 --single-branch https://github.com/immortalwrt/packages.git package/immortal-pkg
+mv package/immortal-pkg/lang/ruby feeds/packages/lang/ruby
+rm -rf package/immortal-pkg
