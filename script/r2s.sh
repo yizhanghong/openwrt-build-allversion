bfbe48694519721fd3448a3623a15a1acb839231
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-18 18:37:34 +0800

    Update r2s.sh

--- script/r2s.sh
+++ script/r2s.sh
@@ -1,6 +1,9 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
+sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
+
 #安装最新openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
