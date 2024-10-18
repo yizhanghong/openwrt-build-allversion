d01628a3e4601099031b5ee56d387d5f981c63c1
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-18 19:02:48 +0800

    Create n100.sh

---
+++ script/n100.sh
@@ -0,0 +1,8 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
+
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
