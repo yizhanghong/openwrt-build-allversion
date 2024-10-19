cbed9d83636da1252888e0c5e7679ca181037dc3
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-19 13:47:52 +0800

    Update r2s.sh

--- script/r2s.sh
+++ script/r2s.sh
@@ -5,8 +5,8 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/SNAPSHOT/V24.10.19/g' package/default-settings/files/zzz-default-settings
 
-# git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-# git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
