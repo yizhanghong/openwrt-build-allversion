a5797f72c382c084960ae74d35415a735059182d
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-25 00:49:23 +0800

    Update lean.sh

--- script/lean.sh
+++ script/lean.sh
@@ -16,11 +16,11 @@ rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
-#git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-sed -i '24d' package/5g-mod/luci-app-modem/Makefile
+
 # iStore
 #git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 #git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
@@ -36,3 +36,8 @@ sed -i '24d' package/5g-mod/luci-app-modem/Makefile
 # Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git package/kenzok8
+mv package/kenzok8/luci-theme-design package/luci-theme-design
+rm -rf package/kenzok8
+
+git clone --depth=1 https://github.com/kiddin9/luci-app-wizard.git  package/luci-app-wizard
