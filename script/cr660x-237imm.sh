75b6c1065b7f06e88f2a21fd9bd91ab329434efd
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-25 13:34:57 +0800

    Update cr660x-237imm.sh

--- script/cr660x-237imm.sh
+++ script/cr660x-237imm.sh
@@ -1,5 +1,5 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/CR6605/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/CR6606/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/luci/applications/luci-app-filetransfer
 rm -rf package/emortal/luci-app-mwan3helper-chinaroute
