4b41096b77e009183d79085656287ec2d01c3eca
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-25 14:54:57 +0800

    Update cr660x-237imm.sh

--- script/cr660x-237imm.sh
+++ script/cr660x-237imm.sh
@@ -8,4 +8,6 @@ rm -rf feeds/luci/themes/luci-theme-argonv3
 rm -rf feeds/luci/themes/luci-theme-argonv2
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-git clone --depth=1 https://github.com/kiddin9/luci-app-wizard.git  package/luci-app-wizard
+git clone --depth=1 https://github.com/sirpdboy/sirpdboy-package.git  package/sirpdboy
+mv package/sirpdboy/luci-app-netwizard package/luci-app-netwizard
+rm -rf package/sirpdboy
