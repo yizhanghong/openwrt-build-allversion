33bb37ee2a982bb8ffcbf8707d98d7567d2c2386
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-28 11:58:58 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -8,11 +8,21 @@ sed -i 's/ImmortalWrt/IPQ6000/g' package/network/config/wifi-scripts/files/lib/w
 
 
 #下载5g模块
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-rm -rf feeds/packages/kernel/fibocom-qmi-wwan
-rm -rf feeds/packages/kernel/quectel-qmi-wwan
+# git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+
 
 rm -rf feeds/packages/lang/ruby
 git clone --depth=1 -b openwrt-23.05 --single-branch https://github.com/immortalwrt/packages.git package/immortal-pkg
 mv package/immortal-pkg/lang/ruby feeds/packages/lang/ruby
 rm -rf package/immortal-pkg
+
+# iStore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
+
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/luci-app-jellyfin package/luci-app-jellyfin
+rm -rf package/kz8-small
