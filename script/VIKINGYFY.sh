35533fd4213baa37e0ab3466695f0f1974bd49b8
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-28 22:37:58 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -1,5 +1,5 @@
 # 修改默认IP，主机名
-sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 #默认WiFi设置
 sed -i 's/ImmortalWrt/IPQ6000/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
@@ -8,8 +8,10 @@ sed -i 's/ImmortalWrt/IPQ6000/g' package/network/config/wifi-scripts/files/lib/w
 
 
 #下载5g模块
-# git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+rm -rf feeds/packages/net/quectel-cm
+rm -rf feeds/packages/kernel/fibocom-qmi-wwan
+rm -rf feeds/packages/kernel/quectel-qmi-wwan
 
 rm -rf feeds/packages/lang/ruby
 git clone --depth=1 -b openwrt-23.05 --single-branch https://github.com/immortalwrt/packages.git package/immortal-pkg
@@ -17,12 +19,12 @@ mv package/immortal-pkg/lang/ruby feeds/packages/lang/ruby
 rm -rf package/immortal-pkg
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-mv package/nas-packages/network/services/* package/nas-packages/
-rm -rf package/nas-packages/network
+#git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+#git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+#git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+#mv package/nas-packages/network/services/* package/nas-packages/
+#rm -rf package/nas-packages/network
 
-git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
-mv package/kz8-small/luci-app-jellyfin package/luci-app-jellyfin
-rm -rf package/kz8-small
+#git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+#mv package/kz8-small/luci-app-jellyfin package/luci-app-jellyfin
+#rm -rf package/kz8-small
