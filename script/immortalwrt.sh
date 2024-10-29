e0da19a696719b1b1d4aef043d68c598e00db83e
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-10-29 21:30:45 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -1,10 +1,10 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/MiR3G/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/ac2100/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/AX6S/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
 
 # git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-sed -i '24d' package/5g-modem/luci-app-modem/Makefile
+#git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+#sed -i '24d' package/5g-modem/luci-app-modem/Makefile
