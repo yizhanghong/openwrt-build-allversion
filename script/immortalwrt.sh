be6c088de8b454c66f5811016f9399ea29fbdb9f
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-01 16:16:37 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -5,7 +5,7 @@ sed -i 's/ImmortalWrt/AX3000/g' package/base-files/files/bin/config_generate
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
 
-git clone --depth=1 https://github.com/sirpdboy/luci-app-netwizard.git package/netwizard
+git clone --depth=1 https://github.com/kiddin9/luci-app-wizard.git package/netwizard
 # git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 #git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 #sed -i '24d' package/5g-modem/luci-app-modem/Makefile
