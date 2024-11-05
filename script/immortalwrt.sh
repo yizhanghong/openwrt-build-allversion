d648df6a73bd26e95c9ccc1d263a5955f5ce833a
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-05 15:51:15 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -1,5 +1,5 @@
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/AX3000/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/AX3000Pro/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/AX6S/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
