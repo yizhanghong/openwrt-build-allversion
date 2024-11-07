8e990b560fa7d13aa45918c9d99b858de416b94b
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-07 22:25:02 +0800

    Update n100.sh

--- script/n100.sh
+++ script/n100.sh
@@ -1,8 +1,14 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/MiR3G/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/x86/g' package/base-files/files/bin/config_generate
 
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
 
-# git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+#git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+
+# iStore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
