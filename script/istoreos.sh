a78de32a0d2b6d309881f953f0a27de85d8dc8e9
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-08 10:38:28 +0800

    Update istoreos.sh

--- script/istoreos.sh
+++ script/istoreos.sh
@@ -1,6 +1,7 @@
 rm -rf feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
+git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-pkg
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
