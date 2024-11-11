5ff2210ab35181f1a96df44747afaa62ad6c4c02
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-11 23:53:24 +0800

    Update x86-imm2305.sh

--- script/x86-imm2305.sh
+++ script/x86-imm2305.sh
@@ -1,5 +1,5 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/R86S/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/N100/g' package/base-files/files/bin/config_generate
 
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
@@ -11,8 +11,8 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/p
 
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-mv package/nas-packages/network/services/* package/nas-packages/
-rm -rf package/nas-packages/network
+#git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+#git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+#git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+#mv package/nas-packages/network/services/* package/nas-packages/
+#rm -rf package/nas-packages/network
