dd51e17c08d54ff64378df568b9aef391fd36579
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-12 00:59:36 +0800

    Update x86-imm2305.sh

--- script/x86-imm2305.sh
+++ script/x86-imm2305.sh
@@ -1,4 +1,5 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/N100/g' package/base-files/files/bin/config_generate
 
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
