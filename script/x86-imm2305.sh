bd0d7eaf3f00f0383e52a8bc078d6cd42e65b8e9
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-14 23:10:49 +0800

    Update x86-imm2305.sh

--- script/x86-imm2305.sh
+++ script/x86-imm2305.sh
@@ -1,6 +1,6 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/ImmortalWrt/N100/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/X86/g' package/base-files/files/bin/config_generate
 
 sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
@@ -9,7 +9,7 @@ sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-defau
 rm -rf feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # iStore
 #git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
