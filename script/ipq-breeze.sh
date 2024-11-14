c624390e564f2e71386b263e89bb27e7c1fc7b2d
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-14 14:28:43 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -17,6 +17,16 @@ sed -i 's/%M/OpenWrt/g' package/base-files/files/usr/lib/os-release
 # mv package/nas-packages/network/services/* package/nas-packages/
 # rm -rf package/nas-packages/network
 
+#下载5g模块
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+#sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
+#sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
+rm -rf feeds/packages/net/quectel-cm
+rm -rf feeds/packages/kernel/fibocom-qmi-wwan
+rm -rf feeds/packages/kernel/quectel-qmi-wwan
+rm -rf feeds/luci/protocols/luci-proto-quectel
+rm -rf feeds/nss_packages/wwan
+
 git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus.git package/eqosplus
 sed -i 's/Control/NetControl/g' package/eqosplus/luasrc/controller/eqosplus.lua
 sed -i '$a msgid "NetControl"' package/eqosplus/po/zh_Hans/eqosplus.po
