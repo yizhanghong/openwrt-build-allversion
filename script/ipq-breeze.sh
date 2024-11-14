8e2f9afc85e7e359d46d2fabfdf0b4a43d27b898
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-14 20:04:33 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -1,6 +1,6 @@
 # 修改默认IP，主机名
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/%d/openwrt/g' package/base-files/files/usr/lib/os-release
 sed -i 's/%D/OpenWrt/g' package/base-files/files/usr/lib/os-release
@@ -18,21 +18,16 @@ sed -i 's/%M/OpenWrt/g' package/base-files/files/usr/lib/os-release
 # rm -rf package/nas-packages/network
 
 #下载5g模块
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+#git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
 #sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
 #sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
 #rm -rf feeds/packages/net/quectel-cm
-rm -rf feeds/packages/kernel/fibocom-qmi-wwan
-rm -rf feeds/packages/kernel/quectel-qmi-wwan
+#rm -rf feeds/packages/kernel/fibocom-qmi-wwan
+#rm -rf feeds/packages/kernel/quectel-qmi-wwan
 #rm -rf feeds/luci/protocols/luci-proto-quectel
 #rm -rf feeds/nss_packages/wwan
 
-git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus.git package/eqosplus
-sed -i 's/Control/NetControl/g' package/eqosplus/luasrc/controller/eqosplus.lua
-sed -i '$a msgid "NetControl"' package/eqosplus/po/zh_Hans/eqosplus.po
-sed -i '$a msgstr "网络控制"' package/eqosplus/po/zh_Hans/eqosplus.po
-sed -i '$a msgid "NetControl"' package/eqosplus/po/zh-cn/eqosplus.po
-sed -i '$a msgid "网络控制"' package/eqosplus/po/zh-cn/eqosplus.po
+
 
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
