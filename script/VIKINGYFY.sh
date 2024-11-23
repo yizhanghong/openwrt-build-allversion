266fa6d63ec4bc215d6a6a11decb3e5f28cdd5e1
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-23 23:57:57 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -2,7 +2,7 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/AX1800/g' package/base-files/files/bin/config_generate
-sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
+
 #默认WiFi设置
 # sed -i 's/ImmortalWrt/AX1800/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i 's/encryption=none/encryption=psk2/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
