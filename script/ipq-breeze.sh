2c13f56fe83a257ca46a0f4efa668fd60f51a6c7
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-11-24 01:39:07 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -2,31 +2,29 @@
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/%d/openwrt/g' package/base-files/files/usr/lib/os-release
-sed -i 's/%D/OpenWrt/g' package/base-files/files/usr/lib/os-release
-sed -i 's/%M/OpenWrt/g' package/base-files/files/usr/lib/os-release
+sed -i 's/LiBwrt/OpenWrt/g' include/version.mk
 # sed -i 's/encryption=none/encryption=psk2/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i '214i\\t\t\tset wireless.default_${name}.key=123456qwerty' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 
 
 # iStore
-# git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 # git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
 # git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
 # mv package/nas-packages/network/services/* package/nas-packages/
 # rm -rf package/nas-packages/network
 
 #下载5g模块
-#git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-#sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
-#sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
-#rm -rf feeds/packages/net/quectel-cm
-#rm -rf feeds/packages/kernel/fibocom-qmi-wwan
-#rm -rf feeds/packages/kernel/quectel-qmi-wwan
-#rm -rf feeds/luci/protocols/luci-proto-quectel
-#rm -rf feeds/nss_packages/wwan
-
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
+sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
+sed -i 's/\"network\"/\"modem\"/g' package/5g-modem/luci-app-modem/luasrc/controller/modem.lua
+rm -rf feeds/packages/net/quectel-cm
+rm -rf feeds/packages/kernel/fibocom-qmi-wwan
+rm -rf feeds/packages/kernel/quectel-qmi-wwan
+rm -rf feeds/luci/protocols/luci-proto-quectel
+rm -rf feeds/nss_packages/wwan
 
 
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
@@ -34,13 +32,4 @@ sed -i 's/%M/OpenWrt/g' package/base-files/files/usr/lib/os-release
 # git clone https://github.com/destan19/OpenAppFilter.git package/mypackage/OpenAppFilter
 # mv package/small/chinadns-ng package/mypackage/
 # rm -rf package/small
-# 添加cpufreq和diskman
-# git clone --depth=1 https://github.com/immortalwrt/luci.git package/immortal-luci
-# mv package/immortal-luci/applications/luci-app-cpufreq package/mypackage/
-# mv package/immortal-luci/applications/luci-app-diskman package/mypackage/
-# mv package/immortal-luci/luci.mk package/mypackage/
-# rm -rf package/immortal-luci
-# sed -i '10d' package/mypackage/luci-app-cpufreq/Makefile
-# sed -i '51d' package/mypackage/luci-app-diskman/Makefile
-# sed -i '10i include ../luci.mk' package/mypackage/luci-app-cpufreq/Makefile
-# sed -i '51i include ../luci.mk' package/mypackage/luci-app-diskman/Makefile
+
