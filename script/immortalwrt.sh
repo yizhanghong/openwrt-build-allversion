0d01768365eb45f1ffcb26c52b5b7a92163e2593
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-04 03:23:10 +0800

    Update immortalwrt.sh

--- script/immortalwrt.sh
+++ script/immortalwrt.sh
@@ -10,4 +10,13 @@ sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emo
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 #git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-#sed -i '24d' package/5g-modem/luci-app-modem/Makefile
+
+git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
+mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/luci-app-fileassistant package/luci-app-fileassistant
+mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
+mv package/kz8-small/luci-app-macvlan package/luci-app-macvlan
+mv package/kz8-small/luci-app-partexp package/luci-app-partexp
+mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+mv package/kz8-small/wrtbwmon package/wrtbwmon
+rm -rf package/kz8-small
