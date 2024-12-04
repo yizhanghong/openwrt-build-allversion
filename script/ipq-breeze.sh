13628d8cb727058686374d6c2dc3aa2cdc0e296d
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-05 00:58:02 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -11,7 +11,7 @@ git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-
 sed -i '/pcie_mhi/d' package/5g-modem/luci-app-modem/Makefile
 #sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
 #sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
-#sed -i 's/\"network\"/\"modem\"/g' package/5g-modem/luci-app-modem/luasrc/controller/modem.lua
+sed -i 's/\"network\"/\"modem\"/g' package/5g-modem/luci-app-modem/luasrc/controller/modem.lua
 rm -rf feeds/packages/net/quectel-cm
 rm -rf feeds/packages/kernel/fibocom-qmi-wwan
 rm -rf feeds/packages/kernel/quectel-qmi-wwan
