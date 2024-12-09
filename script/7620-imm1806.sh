519c72fbf7d522ff9ecd5e2f7faf64e4991c5795
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-09 12:07:02 +0800

    Update 7620-imm1806.sh

--- script/7620-imm1806.sh
+++ script/7620-imm1806.sh
@@ -1,25 +1,25 @@
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/Aimydee/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+mv $GITHUB_WORKSPACE/patch/imm18.06-k5.4/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#
-rm -rf feeds/luci/modules/luci-base/po/zh-cn
-rm -rf feeds/luci/applications/luci-app-passwall/po/zh-cn
-rm -rf feeds/luci/applications/luci-app-timecontrol/po/zh-cn
-rm -rf feeds/luci/applications/luci-app-firewall/po/zh-cn
-rm -rf feeds/luci/applications/luci-app-upnp/po/zh-cn
+#rm -rf feeds/luci/modules/luci-base/po/zh-cn
+#rm -rf feeds/luci/applications/luci-app-passwall/po/zh-cn
+#rm -rf feeds/luci/applications/luci-app-timecontrol/po/zh-cn
+#rm -rf feeds/luci/applications/luci-app-firewall/po/zh-cn
+#rm -rf feeds/luci/applications/luci-app-upnp/po/zh-cn
 
 
 # sed -i 's/bootstrap/design/g' feeds/luci/modules/luci-base/root/etc/config/luci
 # sed -i "s/luci-theme-design/luci-theme-design/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-rm -rf feeds/luci/applications/luci-app-filetransfer
+#rm -rf feeds/luci/applications/luci-app-filetransfer
 
 rm -rf feeds/luci/themes/luci-theme-argonv3
 rm -rf feeds/luci/themes/luci-theme-argonv2
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
-mv package/openwrt-packages/luci-theme-design package/luci-theme-design
-rm -rf package/openwrt-packages
+#git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
+#mv package/openwrt-packages/luci-theme-design package/luci-theme-design
+#rm -rf package/openwrt-packages
 
 # rm -rf feeds/luci/themes/luci-theme-bootstrap
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
