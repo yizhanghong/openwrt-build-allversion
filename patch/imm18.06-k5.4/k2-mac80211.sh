25f38e0b13ceca02e5e814a962d08fe76beaec2c
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-09 12:02:12 +0800

    Update k2-mac80211.sh

--- patch/imm18.06-k5.4/k2-mac80211.sh
+++ patch/imm18.06-k5.4/k2-mac80211.sh
@@ -174,17 +174,15 @@ detect_mac80211() {
 			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
-			set wireless.radio${devidx}.country=RU
+			set wireless.radio${devidx}.country=CN
 			set wireless.radio${devidx}.disabled=0
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio0.ssid==Aimydee-K2-2.4G
-			set wireless.default_radio1.ssid==Aimydee-K2-5G
-			set wireless.default_radio${devidx}.encryption=psk2
-			set wireless.default_radio${devidx}.key=Ta23H64kg
+			set wireless.default_radio${devidx}.ssid=OpenWrt
+			set wireless.default_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
 
