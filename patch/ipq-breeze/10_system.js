d27064af5cb48e2ce602042f0245f1102d73f886
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-16 01:09:19 +0800

    Update 10_system.js

--- patch/ipq-breeze/10_system.js
+++ patch/ipq-breeze/10_system.js
@@ -84,7 +84,7 @@ return baseclass.extend({
 			_('Model'),            boardinfo.model + cpubench.cpubench,
 			_('Architecture'),     cpuinfo.cpuinfo || boardinfo.system,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
+			_('Firmware Version'), 'OpenWrt 23.05-SNAPSHOT on 2024.12.16 / LuCI openwrt-23.05 branch ',
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
