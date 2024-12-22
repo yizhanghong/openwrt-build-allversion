9365ff90d4507b77079987b8f9bab347b67545b1
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-23 01:37:31 +0800

    Update tiktok.sh

--- script/tiktok.sh
+++ script/tiktok.sh
@@ -31,13 +31,12 @@ git clone --depth 1 -b main https://github.com/linkease/nas-packages-luci.git pa
 mv package/nas-packages/network/services/* package/nas-packages/
 rm -rf package/nas-packages/network
 
-git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
+# git clone --depth 1 https://github.com/fw876/helloworld.git package/helloworld
 # git clone --depth 1 https://github.com/morytyann/OpenWrt-mihomo.git package/mihomo
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 rm -rf feeds/packages/devel/gn
-mv package/passwall-packages/gn feeds/packages/devel/gn
-rm -rf package/passwall-packages
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 mv package/kz8-small/luci-app-fileassistant package/luci-app-fileassistant
