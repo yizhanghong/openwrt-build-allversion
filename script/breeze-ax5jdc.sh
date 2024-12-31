ef02fc402144662d26d32b6b72ca49fa8690e2d2
Siha06 <65899583+Siha06@users.noreply.github.com>
2024-12-31 18:31:44 +0800

    Update and rename lean.sh to breeze-ax5jdc.sh

---
+++ script/breeze-ax5jdc.sh
@@ -0,0 +1,8 @@
+# 修改默认IP，主机名
+sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LiBwrt/OpenWrt/g' include/version.mk
+
+mv $GITHUB_WORKSPACE/patch/ipq-breeze/mac80211.uc package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+mv $GITHUB_WORKSPACE/patch/ipq-breeze/home.wuhe.sh $OPENWRT_PATH/package/base-files/files/etc/uci-defaults/home.wuhe.sh
