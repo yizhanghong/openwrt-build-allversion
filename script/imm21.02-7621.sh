79afc1574f381c8a016913f635878713be925498
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-01 00:38:49 +0800

    Update imm21.02-7621.sh

--- script/imm21.02-7621.sh
+++ script/imm21.02-7621.sh
@@ -20,27 +20,6 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclas
 mv package/openclash/luci-app-openclash feeds/luci/applications/
 rm -rf package/openclash
 
-# 添加kenzok8_small插件库, 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本 ，可以用以下命令
-rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/kenzok8/golang feeds/packages/lang/golang
-#删除自带的老旧依赖，ssr-plus，passwall
-rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy}
-rm -rf feeds/packages/net/{pdnsd-alt,simple-obfs,sing-box,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns,redsocks2}
-rm -rf feeds/packages/net/{ssocks,shadow-tls}
-rm -rf feeds/packages/devel/gn
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-mosdns}
-
-#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
-
-#新版ssr-libev和ss-rust缺少依赖，所以延用旧版，删除新版
-rm -rf package/passwall-packages/shadowsocks-rust
-rm -rf package/passwall-packages/shadowsocksr-libev
-rm -rf package/helloworld/shadowsocks-rust
-rm -rf package/helloworld/shadowsocksr-libev
 
 # iStore
 git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/mypackage/nas-packages
