f248f2da86d87646b70c819fc33b02daba211440
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-03 20:21:31 +0800

    Update ipq-ax5jdc.sh

--- script/ipq-ax5jdc.sh
+++ script/ipq-ax5jdc.sh
@@ -4,5 +4,5 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/l
 sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LiBwrt/OpenWrt/g' include/version.mk
 
-mv $GITHUB_WORKSPACE/patch/ipq-breeze/mac80211.uc package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-mv $GITHUB_WORKSPACE/patch/ipq-breeze/home.wuhe.sh $OPENWRT_PATH/package/base-files/files/etc/uci-defaults/home.wuhe.sh
+mv $GITHUB_WORKSPACE/patch/ipq-breeze/home/mac80211.uc package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+mv $GITHUB_WORKSPACE/patch/ipq-breeze/home/wuhe.sh $OPENWRT_PATH/package/base-files/files/etc/uci-defaults/home.wuhe.sh
