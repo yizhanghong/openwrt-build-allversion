3d8354496c8d1bb7007c82e8345473e027b30c1d
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-19 14:32:22 +0800

    Update mac80211.sh

--- patch/imm21.02/mac80211.sh
+++ patch/imm21.02/mac80211.sh
@@ -181,8 +181,10 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio0.ssid=WDSLR-2.4G-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }')
+			set wireless.default_radio1.ssid=WDSLR-5G-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }')
+			set wireless.default_radio${devidx}.encryption=psk2+ccmp
+			set wireless.default_radio${devidx}.key=11111111
 EOF
 		uci -q commit wireless
 
