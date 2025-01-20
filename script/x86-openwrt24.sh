a37430bc18dad0787e71ee28dd9c21209b5ec79d
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-21 01:08:16 +0800

    Update x86-openwrt24.sh

--- script/x86-openwrt24.sh
+++ script/x86-openwrt24.sh
@@ -2,6 +2,10 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 mv $GITHUB_WORKSPACE/patch/openwrt-24.10/199-mydef.sh package/base-files/files/etc/uci-defaults/199-mydef.sh
 
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
+sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
+
 # iStore
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
