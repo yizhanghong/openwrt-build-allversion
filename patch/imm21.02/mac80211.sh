e7ccb6edb1c6115854b677840d60dd9afbde37c3
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-21 01:58:38 +0800

    Update mac80211.sh

--- patch/imm21.02/mac80211.sh
+++ patch/imm21.02/mac80211.sh
@@ -175,7 +175,8 @@ detect_mac80211() {
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
 			set wireless.radio${devidx}.country=CN
-			set wireless.radio${devidx}.disabled=0
+			set wireless.radio0.disabled=1
+			set wireless.radio1.disabled=0
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
