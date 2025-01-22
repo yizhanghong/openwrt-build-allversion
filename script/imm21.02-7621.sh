a409dc2825766a2c144deafb74329229c3ab5eb5
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-22 20:43:22 +0800

    Update imm21.02-7621.sh

--- script/imm21.02-7621.sh
+++ script/imm21.02-7621.sh
@@ -1,9 +1,13 @@
-sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.101.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
+#sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
+#sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.101.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+mv $GITHUB_WORKSPACE/patch/imm21.02/config_generate package/base-files/files/bin/config_generate
+
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/imm21.02/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#mv $GITHUB_WORKSPACE/patch/imm21.02/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/imm21.02/199-mydef.sh package/base-files/files/etc/uci-defaults/199-mydef.sh
 mv $GITHUB_WORKSPACE/patch/imm21.02/virtualhere package/base-files/files/etc/virtualhere
