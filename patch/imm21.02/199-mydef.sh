aab0265b2f108b7aff90f14d0cbfa44c374eabef
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-22 21:17:33 +0800

    Update 199-mydef.sh

--- patch/imm21.02/199-mydef.sh
+++ patch/imm21.02/199-mydef.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+#uci set wireless.radio0.cell_density=0
 uci set wireless.default_radio0.ssid=WDSLR-5G-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }')
 uci set wireless.default_radio1.ssid=WDSLR-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }')
+uci set wireless.default_radio0.encryption='psk2'
 uci set wireless.default_radio0.key='11111111'
+#uci set wireless.radio0.channel='149'
 uci set network.lan.ipaddr='192.168.101.1'
+uci set system.cfg01e48a.hostname=dslr-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }')
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
@@ -30,5 +34,6 @@ sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 /etc/init.d/network restart
 /etc/init.d/odhcpd restart
 /etc/init.d/rpcd restart
+/etc/init.d/system restart
 
 exit 0
