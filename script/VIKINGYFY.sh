4e18eb8ddefd2245dff793e081209ceb9ca36ca9
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-22 15:53:27 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -1,9 +1,16 @@
 # 修改默认IP，主机名
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/ImmortalWrt/AX1800/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.33.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
+#mv $GITHUB_WORKSPACE/patch/ipq-breeze/199-mydef.sh package/base-files/files/etc/uci-defaults/199-mydef.sh
+#sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-distfeeds.conf
 
 #默认WiFi设置
+sed -i 's/OWRT/OpenWrt-WiFi/g' package/base-files/files/etc/uci-defaults/990_set-wireless.sh
+sed -i '/BASE_WORD/d' package/base-files/files/etc/uci-defaults/990_set-wireless.sh
+sed -i 's/psk2+ccmp/none/g' package/base-files/files/etc/uci-defaults/990_set-wireless.sh
 # sed -i 's/ImmortalWrt/AX1800/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i 's/encryption=none/encryption=psk2/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 # sed -i '214i\\t\t\tset wireless.default_${name}.key=123456qwerty' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
@@ -27,15 +34,15 @@ rm -rf feeds/nss_packages/wwan
 
 # iStore
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-#git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-#git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-#mv package/nas-packages/network/services/* package/nas-packages/
-#rm -rf package/nas-packages/network
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
 
-git clone --depth=1 https://github.com/ZeaKyX/luci-app-speedtest-web.git package/luci-app-speedtest-web
-#git clone --depth=1 https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
+git clone --depth=1 https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
+mv package/kz8-small/luci-app-easymesh package/luci-app-easymesh
 mv package/kz8-small/luci-app-fileassistant package/luci-app-fileassistant
 mv package/kz8-small/luci-app-ikoolproxy package/luci-app-ikoolproxy
 mv package/kz8-small/luci-app-macvlan package/luci-app-macvlan
@@ -43,3 +50,6 @@ mv package/kz8-small/luci-app-partexp package/luci-app-partexp
 mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
 mv package/kz8-small/wrtbwmon package/wrtbwmon
 rm -rf package/kz8-small
+
+chmod +x package/emortal/luci-app-athena-led/root/etc/init.d/athena_led 
+chmod +x package/emortal/luci-app-athena-led/root/usr/sbin/athena-led
