582b6c1ef909996155e9fabb73d1c71846bb0e1b
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-22 21:18:32 +0800

    Update imm21.02-7621.sh

--- script/imm21.02-7621.sh
+++ script/imm21.02-7621.sh
@@ -1,8 +1,6 @@
 #sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
 #sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.101.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-mv $GITHUB_WORKSPACE/patch/imm21.02/config_generate package/base-files/files/bin/config_generate
 
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #mv $GITHUB_WORKSPACE/patch/imm21.02/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
