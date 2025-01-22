f022a054e2b22a1be438b327347511795cb90743
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-01-23 02:46:15 +0800

    Update 199-mydef.sh

--- patch/ipq-breeze/199-mydef.sh
+++ patch/ipq-breeze/199-mydef.sh
@@ -6,8 +6,6 @@ echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 echo "DISTRIB_DESCRIPTION='OpenWrt-V${date_version} '" >> /etc/openwrt_release
 
-sed -i '/ula_prefix/d' /etc/config/network
-sed -i 's/60/64/g' /etc/config/network
 sed -i '$a src/gz openwrt_others https://mirror.nju.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/qualcommax/ipq807x/packages' /etc/opkg/customfeeds.conf
 sed -i '$a src/gz openwrt_kmods https://mirror.nju.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/qualcommax/ipq807x/kmods/6.6.65-1-c9824125bf4a8028af70bed3c9a6ae77' /etc/opkg/customfeeds.conf
 
@@ -16,6 +14,9 @@ uci delete ttyd.@ttyd[0].interface
 
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
+
+uci set network.lan.ip6assign=64
+uci del network.globals.ula_prefix
 uci commit
 
 # wifi设置
