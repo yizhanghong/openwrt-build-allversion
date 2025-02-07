39462b81dba63eeb5e4b0ab62818cb63560c3920
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-07 19:00:53 +0800

    Update ARM64-openwrt.sh

--- script/ARM64-openwrt.sh
+++ script/ARM64-openwrt.sh
@@ -1,5 +1,9 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 mv $GITHUB_WORKSPACE/patch/openwrt-24.10/199-arm64.sh package/base-files/files/etc/uci-defaults/199-mydef.sh
 
+#完全删除luci版本
+sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 #添加编译日期
 sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/usr/lib/os-release
 sed -i "s/%C/\/ Complied on $(date +"%Y.%m.%d")/g" package/base-files/files/etc/openwrt_release
