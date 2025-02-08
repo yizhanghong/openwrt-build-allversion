df71497880eb80eb3160d4bf3a9c699e39ceacea
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-08 13:33:05 +0800

    Update ipq-breeze.sh

--- script/ipq-breeze.sh
+++ script/ipq-breeze.sh
@@ -1,6 +1,6 @@
 # 修改默认IP，主机名
-sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/LibWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LiBwrt/OpenWrt/g' include/version.mk
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
