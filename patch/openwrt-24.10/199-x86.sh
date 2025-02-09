eda406d2f93e5535858c00b6c56a69a7544d1c99
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-09 17:27:57 +0800

    Update 199-x86.sh

--- patch/openwrt-24.10/199-x86.sh
+++ patch/openwrt-24.10/199-x86.sh
@@ -3,32 +3,6 @@
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 #uci set firewall.@zone[1].input='ACCEPT'
 
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-#uci add dhcp domain
-#uci set "dhcp.@domain[-1].name=time.android.com"
-#uci set "dhcp.@domain[-1].ip=203.107.6.88"
-
-
-# 根据网卡数量配置网络
-count=0
-for iface in /sys/class/net/*; do
-  iface_name=$(basename "$iface")
-  # 检查是否为物理网卡（排除回环设备和无线设备）
-  if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
-    count=$((count + 1))
-  fi
-done
-
-# 网络设置
-if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
-  uci set dhcp.lan.ignore='1'
-elif [ "$count" -gt 1 ]; then
-  uci set network.wan.device='eth0'
-  uci del network.cfg030f15.ports
-  uci add_list network.cfg030f15.ports='eth1'
-fi
-
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
