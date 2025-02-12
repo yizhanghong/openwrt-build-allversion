162c09c7ff6fb6b0a9b1eb4ff40d037fcf967f35
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-12 14:29:40 +0800

    Update 7915-237.sh

--- script/7915-237.sh
+++ script/7915-237.sh
@@ -1,9 +1,9 @@
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/Bitray/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # sed -i 's/bootstrap/design/g' feeds/luci/modules/luci-base/root/etc/config/luci
 # sed -i "s/luci-theme-design/luci-theme-design/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-rm -rf feeds/luci/applications/luci-app-filetransfer
+#rm -rf feeds/luci/applications/luci-app-filetransfer
 rm -rf package/emortal/luci-app-mwan3helper-chinaroute
 
 rm -rf feeds/luci/themes/luci-theme-argonv3
@@ -13,7 +13,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone --depth 1 https://github.com/kenzok8/golang feeds/packages/lang/golang
 #删除自带的老旧依赖，ssr-plus，passwall
 rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,hysteria,ipt2socks,microsocks,naiveproxy}
 rm -rf feeds/packages/net/{pdnsd-alt,simple-obfs,sing-box,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns,redsocks2}
