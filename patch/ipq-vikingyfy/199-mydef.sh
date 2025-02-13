956982667a109eb265f875cfce1866435e745309
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-13 20:09:43 +0800

    Update 199-mydef.sh

--- patch/ipq-vikingyfy/199-mydef.sh
+++ patch/ipq-vikingyfy/199-mydef.sh
@@ -2,8 +2,9 @@
 
 sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' etc/apk/repositories.d/distfeeds.list
 #sed -i '$a src/gz openwrt_others https://mirror.nju.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/packages' /etc/apk/repositories.d/customfeeds.list
-sed -i '$a src/gz openwrt_kmods https://mirror.nju.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.71-1-ff75615298a003c0b4ac7104bd8904ff' /etc/apk/repositories.d/customfeeds.list
+sed -i '$a src/gz openwrt_kmods https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.77-1-9ff595d0bc717803d56fb3b718a00292' /etc/apk/repositories.d/customfeeds.list
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
