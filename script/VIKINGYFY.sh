9a95e8633bdda0648caa4ce752f87eaa44d30b1d
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-17 00:31:43 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -1,6 +1,6 @@
 # 修改默认IP，主机名
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.33.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
