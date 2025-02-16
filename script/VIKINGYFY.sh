ac7f351cb22ff28c40080c96e72aac23178b955f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-17 01:23:13 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -2,6 +2,7 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/ipq-vikingyfy/998-mydef.sh package/base-files/files/etc/uci-defaults/998-mydef.sh
