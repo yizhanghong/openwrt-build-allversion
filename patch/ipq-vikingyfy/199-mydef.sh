5e0e26d4a091484d39ccb4f6b951520fa56db04f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-17 00:56:29 +0800

    Update 199-mydef.sh

--- patch/ipq-vikingyfy/199-mydef.sh
+++ patch/ipq-vikingyfy/199-mydef.sh
@@ -2,7 +2,7 @@
 
 sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
-sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
 sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.77-1-51630055abc52e4640365e0d2a28a72e/packages.adb' /etc/apk/repositories.d/customfeeds.list
 
 # 设置所有网口可访问网页终端
