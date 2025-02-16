385db313289224f7ae19fda3fecf88fc87058afa
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-17 01:50:29 +0800

    Create 998-ipq807x.sh

---
+++ patch/ipq-vikingyfy/998-ipq807x.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sed -i '/nss/d' /etc/apk/repositories.d/distfeeds.list
+sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq807x/kmods/6.6.77-1-750dd8effa34f99140df2bf38b237251/packages.adb' /etc/apk/repositories.d/customfeeds.list
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+# wifi设置
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+
+uci commit
+
+#/etc/init.d/network restart
+
+exit 0
