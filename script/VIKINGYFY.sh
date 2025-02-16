05b35b062e36fa9dd61ce3a42e34399ac0e4db95
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-17 00:28:49 +0800

    Update VIKINGYFY.sh

--- script/VIKINGYFY.sh
+++ script/VIKINGYFY.sh
@@ -1,5 +1,5 @@
 # 修改默认IP，主机名
-sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.33.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
@@ -51,5 +51,5 @@ mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
 mv package/kz8-small/wrtbwmon package/wrtbwmon
 rm -rf package/kz8-small
 
-chmod +x package/emortal/luci-app-athena-led/root/etc/init.d/athena_led 
-chmod +x package/emortal/luci-app-athena-led/root/usr/sbin/athena-led
+#chmod +x package/emortal/luci-app-athena-led/root/etc/init.d/athena_led 
+#chmod +x package/emortal/luci-app-athena-led/root/usr/sbin/athena-led
