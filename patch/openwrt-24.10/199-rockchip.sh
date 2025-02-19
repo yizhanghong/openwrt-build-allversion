161a4d5e83f1489539abb134bdf459c22c869b29
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-20 01:53:19 +0800

    Update 199-rockchip.sh

--- patch/openwrt-24.10/199-rockchip.sh
+++ patch/openwrt-24.10/199-rockchip.sh
@@ -7,6 +7,11 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
+#其他网络设置
+uci del wireless.radio0.disabled
+uci del wireless.default_radio0.disabled
+uci commit wireless
+
 uci commit
 
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
