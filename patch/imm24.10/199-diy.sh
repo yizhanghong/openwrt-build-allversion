87e2b4a4bfff47d5f8d42cfd8b989adec8b58a70
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-20 11:23:11 +0800

    Update 199-diy.sh

--- patch/imm24.10/199-diy.sh
+++ patch/imm24.10/199-diy.sh
@@ -8,18 +8,14 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 
 #其他网络设置
-uci del wireless.radio0.disabled
-uci del wireless.default_radio0.disabled
-uci commit wireless
 
 uci commit
 
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
-sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/24.10.0/targets/rockchip/armv8/kmods/6.6.73-1-f35e93bc2c89b98d107e57cdea041972' /etc/opkg/customfeeds.conf
-sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+#sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.0-rc3/targets/rockchip/armv8/kmods/6.6.67-1-e4b0ddba783aa46d81253ebb31af2843' /etc/opkg/customfeeds.conf
+#sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
-wifi up
-/etc/init.d/network restart
+#/etc/init.d/network restart
 
 exit 0
