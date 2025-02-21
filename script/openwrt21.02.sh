088258104d9f6496a9c9e62335383191614fce61
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-21 20:43:20 +0800

    Update openwrt21.02.sh

--- script/openwrt21.02.sh
+++ script/openwrt21.02.sh
@@ -1,7 +1,7 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.8.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-mv $GITHUB_WORKSPACE/patch/imm21.02/299-tempdiy package/base-files/files/etc/uci-defaults/299-tempdiy.sh
-mv $GITHUB_WORKSPACE/patch/imm21.02/8929 package/base-files/files/etc/8929
+mv $GITHUB_WORKSPACE/patch/imm21.02/299-tempdiy.sh package/base-files/files/etc/uci-defaults/299-tempdiy.sh
+mv $GITHUB_WORKSPACE/patch/imm21.02/8929.gz package/base-files/files/etc/8929.gz
 
 #完全删除luci版本
 sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
