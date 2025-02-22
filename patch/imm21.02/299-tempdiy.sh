cc23525ca455ddedae96c1b0202a7f568c836ae9
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-02-23 01:16:28 +0800

    Update 299-tempdiy.sh

--- patch/imm21.02/299-tempdiy.sh
+++ patch/imm21.02/299-tempdiy.sh
@@ -5,7 +5,6 @@ uci delete ttyd.@ttyd[0].interface
 
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
-uci commit
 
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
@@ -14,14 +13,11 @@ uci del wireless.radio0.disabled
 uci del wireless.default_radio0.disabled
 uci del wireless.radio1.disabled
 uci del wireless.default_radio1.disabled
+uci commit
 
-mkdir /etc/diy
-mv /etc/8929.gz /etc/diy/8929.gz
-tar -xf /etc/diy/8929.gz
-cp -rf /etc/diy/etc/* /etc/
-cp -rf /etc/diy/root/* /root/
-cp -rf /etc/diy/usr/bin/* /usr/bin/
-rm -rf /etc/diy
+mkdir /root/diy
+mv /etc/8929.gz /root/diy/8929.gz
+sysupgrade --restore-backup /root/diy/8929.gz
 
 wifi up
 /etc/init.d/network restart
